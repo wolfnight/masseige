e6435f77cf05e40bf5ea2a66d6e7c9250bae3d2e
wolfnight <www.mehdi.killer78@gmail.com>
2016-07-03 04:43:58 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '214774174:AAHue5gmlbfrorrXvGzz93eg7z8AS9nytIk');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
